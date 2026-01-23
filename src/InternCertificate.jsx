31179e7be6722a5862c8f623de99c2b4e9619cca
shan <shanmuganathan232007@gmail.com>
2026-01-23 23:24:01 +0530

    Add BackToHome

--- src/InternCertificate.jsx
+++ src/InternCertificate.jsx
@@ -1,48 +1,41 @@
-import certificate from './assets/Zenvy Certificate.png';
-import { useReactToPrint } from 'react-to-print';
-import { useRef } from 'react';
+import { useRef, useState } from "react";
+import { useReactToPrint } from "react-to-print";
+import zenCertificate from './assets/Zenvy Certificate.png'
+
 
 function Certificate() {
-
-  const printRef = useRef();
+  const printRef = useRef(null);
+  const [name,setName] = useState("SHANMUGANATHAN S");
+  const [domain,setDomain] = useState("Mern Stack");
+  const [start_date,setStart_date] = useState("01/01/2026");
+  const [end_date,setEnd_date] = useState("01/01/2026");
 
   const handlePrint = useReactToPrint({
-  content: () => {
-    alert("CONTENT FUNCTION CALLED");
-    console.log("PRINT REF:", printRef.current);
-    return printRef.current;
-  },
-  documentTitle: "Zenvy Certificate",
-});
-
-
+    contentRef: printRef,              
+    documentTitle: "Zenvy Certificate",
+    removeAfterPrint: true,
+    onBeforePrint: async () => {
+      console.log("Printing...", printRef.current);
+    },
+    onAfterPrint: () => {
+      console.log("Done");
+    },
+  });
 
   return (
-    <>
-      {/* PRINT AREA */}
-      <div className='certificate-container' ref={printRef}>
-        <img src={certificate} alt="Certificate" />
-
-        {/* Name on certificate */}
-        <input
-          type="text"
-          className='username'
-          value="SHANMUGANATHAN S"
-          readOnly
-        />
-
-        {/* Domain */}
-        <input
-          type="text"
-          className='domain'
-          placeholder="Web Development"
-          readOnly
-        />
+    <div>
+      <div ref={printRef} className="certificate-container">
+        <img src={zenCertificate} alt="" />
+        <input type="text" className="username" value={name} readOnly />
+        <input type="text" className="domain" value={domain} readOnly />
+        <input type="text" className="start-date" value={start_date} readOnly />
+        <input type="text" className="end-date" value={end_date} readOnly />
       </div>
 
-      {/* PRINT BUTTON */}
-      <button onClick={()=>{console.log("clicked");handlePrint}}>Print</button>
-    </>
+      <button onClick={handlePrint} style={{ marginTop: 10 }}>
+        Print PDF
+      </button>
+    </div>
   );
 }
 
--- src/UserIndex.jsx
+++ src/UserIndex.jsx
@@ -1,4 +1,5 @@
 import AboutUs from "./AboutUs";
+import BackToHome from "./backtohome";
 import Contact from "./Contact";
 import FeedbackPage from "./FeedBackPage";
 import Footer from "./Footer";
@@ -20,6 +21,7 @@ function UserIndex () {
         <FeedbackPage/>
         <Contact/>
         <Footer/>
+        <BackToHome/>
         </>
     )
 }
