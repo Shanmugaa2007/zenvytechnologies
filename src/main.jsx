f2ca58d42fe0457c33eb0a528dc9194f706a3ffb
Shanmuganathan <shanmuganathan232007@gmail.com>
2026-02-03 19:00:06 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
-import { createRoot } from 'react-dom/client'
-import { RouterProvider,createBrowserRouter } from 'react-router-dom'
-import './index.css'
-import UserIndex from './UserIndex'
-import Founders from './Founders'
-import GotoRegister from './gotoRegister'
-import StudentRegister from './StudentRegistration'
-import UserRegistration from './otherUsersregistration'
-import Login from './Login'
-import PrivacyPolicy from './privacypolicy'
-import Certificate from './InternCertificate'
-import ProtectedRoute from './logout'
-import InternIndex from './InternIndex'
-import LearnWithUs from './learnwithus'
-import Dashboard from './InternDashboard'
+import { createRoot } from 'react-dom/client';
+import { RouterProvider,createBrowserRouter } from 'react-router-dom';
+import './index.css';
+import UserIndex from './UserIndex';
+import Founders from './Founders';
+import GotoRegister from './gotoRegister';
+import StudentRegister from './StudentRegistration';
+import UserRegistration from './otherUsersregistration';
+import Login from './Login';
+import PrivacyPolicy from './privacypolicy';
+import Certificate from './InternCertificate';
+import ProtectedRoute from './logout';
+import InternIndex from './InternIndex';
+import LearnWithUs from './learnwithus';
+import Dashboard from './InternDashboard';
 
 
 
