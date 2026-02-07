07e80eb56eebcc89a49d6d447182a81aaea55f95
Shanmuganathan <shanmuganathan232007@gmail.com>
2026-02-07 17:55:06 +0530

    Update InternDashboard.jsx

--- src/InternDashboard.jsx
+++ src/InternDashboard.jsx
@@ -11,7 +11,7 @@ function Dashboard() {
         const [username,setusername] = useState("");
         const fileInputRef = useRef(null);
 
-        axios.get('https://zenvytechnologiess.onrender.com/current-user')
+        axios.get('https://zenvytechnologiess.onrender.com/me')
         .then((res)=>{
         setusername(res.data.user)
         console.log(username)
@@ -72,6 +72,7 @@ function Dashboard() {
 }
 
 export default Dashboard;
+
 
 
 
