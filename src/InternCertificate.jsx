8fde53e886e033c09ed1679b764a7b894ab88377
Your Name <your-email@example.com>
2026-03-03 19:40:00 +0530

    change print in mobile

--- src/InternCertificate.jsx
+++ src/InternCertificate.jsx
@@ -28,53 +28,55 @@ function Certificate({ domain }) {
   const handlePrint = useReactToPrint({
     content: () => printRef.current,
     documentTitle: "Zenvy Certificate",
-    removeAfterPrint: true,
   });
 
   return (
-    <>
+    <div style={{ overflowY: "hidden", overflowX: "hidden" }}>
+      
+      <div ref={printRef} className="certificate-container">
+        <img src={zenCertificate} alt="" />
+        <input type="text" className="username" value={name} readOnly />
+        <input type="text" className="domain" value={domain} readOnly />
+        <input type="text" className="start-date" value={start_date} readOnly />
+        <input type="text" className="end-date" value={end_date} readOnly />
+        <input type="text" className="certificate-no" value={certificateNo} readOnly />
+      </div>
+
+      <div className="hide-certificate"></div>
+
+      <button
+        onClick={() => {
+          handlePrint();
+          setPrintCertificate(true);
+        }}
+        className="print-pdf"
+      >
+        Print Certificate
+      </button>
+
       <style>
         {`
           @media print {
             body * {
-              visibility: hidden;
+              display: none !important;
             }
-            .certificate-container, .certificate-container * {
-              visibility: visible;
+
+            .certificate-container,
+            .certificate-container * {
+              display: block !important;
             }
+
             .certificate-container {
-              position: absolute;
-              left: 0;
+              position: fixed;
               top: 0;
+              left: 0;
               width: 100%;
             }
           }
         `}
       </style>
 
-      <div style={{ overflowY: "hidden", overflowX: "hidden" }}>
-        <div ref={printRef} className="certificate-container">
-          <img src={zenCertificate} alt="" />
-          <input type="text" className="username" value={name} readOnly />
-          <input type="text" className="domain" value={domain} readOnly />
-          <input type="text" className="start-date" value={start_date} readOnly />
-          <input type="text" className="end-date" value={end_date} readOnly />
-          <input type="text" className="certificate-no" value={certificateNo} readOnly />
-        </div>
-
-        <div className="hide-certificate"></div>
-
-        <button
-          onClick={() => {
-            handlePrint();
-            setPrintCertificate(true);
-          }}
-          className="print-pdf"
-        >
-          Print Certificate
-        </button>
-      </div>
-    </>
+    </div>
   );
 }
 
