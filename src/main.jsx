209a0777e50c1f5407fe35211d430cd560bbe44d
Shanmuganathan <shanmuganathan232007@gmail.com>
2026-05-31 16:16:59 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { RouterProvider,createBrowserRouter } from 'react-router-dom'
 import './index.css'
 import UserIndex from './UserIndex'
 import Founders from '../src/Components/Founder/Founders'
+import GotoRegister from '../src/Pages/gotoregister/gotoRegister'
+import StudentRegister from '../src/Pages/Register/StudentRegistration'
+import UserRegistration from '../src/Pages/Register/otherUsersregistration'
+import Login from '../src/Pages/Login/Login'
 import VisionMission from './Components/About/Vision&Mission'
 import WebDevelopment from './Components/Service/wedevelopment'
 import ServiceDetails from './Components/Service/servicedetails'
@@ -12,6 +16,13 @@ import IsoContact from './Components/Contact/isolatedcontact'
 import WebPortfolio from './Components/Service/portfolio/webdevelopportfolio'
 import GraphicsPortfolio from './Components/Service/portfolio/GraphicsPortfolio'
 import DigitalMarketingPortfolio from './Components/Service/portfolio/DigitalMarketingPortfolio'
+import PortfolioSamples from './Components/Service/samples/portfoliosamples'
+import ResumeSamples from './Components/Service/samples/ResumeSamples'
+import UIUXPortfolio from './Components/Service/portfolio/UiUXportfolio'
+import UIDesign from './Components/Service/Ui&Uxdesign'
+import GraphicsDesign from './Components/Service/graphicsdesign'
+import DigitalMarketing from './Components/Service/digitalmarketing'
+import MaintenanceSupport from './Components/Maintenance&Support/Maintenance&support'
 
 
 const router = createBrowserRouter([
@@ -24,15 +35,35 @@ const router = createBrowserRouter([
         <UserIndex />
   },
   {
-    path : '/founders',
+    path : 'founders',
     element : <Founders/>
+  },
+  {
+    path : 'gotoregister',
+    element : <GotoRegister/>
+  },
+  {
+    path : 'gotoregister/studentregister',
+    element : <StudentRegister/>
+  },
+  {
+    path : 'gotoregister/userregister',
+    element : <UserRegistration/>
+  },
+  {
+    path : 'login',
+    element : <Login/>
   },
   {
     path:'/contact-zenvyteam',
     element:<IsoContact/>
   },
   {
-    path:'/zenvytechnologies-aboutus',
+    path : 'gotoregister/login',
+    element : <Login/>
+  },
+  {
+    path:'zenvytechnologies-aboutus',
     element:<VisionMission/>
   },
   {
@@ -40,8 +71,24 @@ const router = createBrowserRouter([
     element:<WebDevelopment/>
   },
   {
+    path:'/service-uiuxdesign',
+    element:<UIDesign/>
+  },
+  {
+    path:'/service-graphicsdesign',
+    element:<GraphicsDesign/>
+  },
+  {
+    path:'/service-digitalmarketing',
+    element:<DigitalMarketing/>
+  },
+  {
     path:'/services/:slug',
     element:<ServiceDetails/>
+  },
+  {
+    path:'/zenvytech-maintenance&support',
+    element:<MaintenanceSupport/>
   },
   {
     path:'/zenvytechnologies-freeconsultation',
@@ -58,6 +105,18 @@ const router = createBrowserRouter([
   {
     path:'/digitalmarketing-portfolio',
     element:<DigitalMarketingPortfolio/>
+  },
+  {
+    path:'/portfoliosamples',
+    element:<PortfolioSamples/>
+  },
+  {
+    path:'/resumesamples',
+    element:<ResumeSamples/>
+  },
+  {
+    path:'/uiuxportfolio',
+    element:<UIUXPortfolio/>
   }
   ]}
 
